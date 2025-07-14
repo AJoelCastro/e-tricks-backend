11b00e80aca243af85a71e80df166ab23de0bb50
AJoelCastro <misthorn33@gmail.com>
2025-07-14 00:45:23 -0500

    routes

--- src/routes/Product.ts
+++ src/routes/Product.ts
@@ -10,6 +10,8 @@ const router = express.Router();
 
 router.get('/get', getProducts);
 router.get('/:id/get', getProductById);
-router.post('/create', createProduct);
+router.post('/create',authenticateClerkToken, createProduct);
+router.put('/:id/update',authenticateClerkToken, getProductById);
+router.delete('/:id/delete',authenticateClerkToken, getProductById);
 
 export default router;
