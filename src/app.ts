ef54ef1857ddb279efa86da078e8a5392749f701
AJoelCastro <misthorn33@gmail.com>
2025-07-18 03:07:03 -0500

    git

--- src/app.ts
+++ src/app.ts
@@ -52,7 +52,7 @@ app.use((err: ErrorWithStatus, req: Request, res: Response, next: NextFunction) 
     });
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = parseInt(process.env.PORT || '4000', 10);
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
