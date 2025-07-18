0d1a5892339dcdf229e6ff8edccfe22e5e570bca
AJoelCastro <misthorn33@gmail.com>
2025-07-18 03:02:34 -0500

    port

--- src/app.ts
+++ src/app.ts
@@ -53,7 +53,7 @@ app.use((err: ErrorWithStatus, req: Request, res: Response, next: NextFunction) 
 });
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
 export default app;
