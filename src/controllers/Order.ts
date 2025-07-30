25e5150451bb4812ddcd020e7d37d540a2cddfc6
marck-h-cmd <mahermenegildopa@unitru.edu.pe>
2025-07-29 23:57:21 -0500

    set back_urls

--- src/controllers/Order.ts
+++ src/controllers/Order.ts
@@ -98,14 +98,14 @@ export const createPreference = async (req: Request, res: Response): Promise<voi
             items,
             // URLs de retorno para el checkout directo
             
-        /*    back_urls: {
+           back_urls: {
                 success: `${process.env.FRONTEND_URL}/carrito/order/success`,
                 failure: `${process.env.FRONTEND_URL}/carrito/order/failure`, 
                 pending: `${process.env.FRONTEND_URL}/carrito/order/pending`
             },
-            auto_return: "approved",   */
+            auto_return: "approved",   
             external_reference: userId, 
-        //    notification_url: `${process.env.BACKEND_URL}/api/orders/webhook`, // Webhook para notificaciones
+            notification_url: `${process.env.BACKEND_URL}/api/orders/webhook`, // Webhook para notificaciones
             statement_descriptor: "TRICKS", // Aparece en el estado de cuenta
             // Configuraciones adicionales
             expires: false, // La preferencia no expira
